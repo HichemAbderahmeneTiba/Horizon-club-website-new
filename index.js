f33e466abea452f4a093c6fc9cc379c7427868ce
hichem Tiba <hichemabderahmenetiba@gmail.com>
2025-02-18 20:51:06 +0100

    core

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const close = document.querySelector('.close-btn');
 const menu = document.querySelector('.menu-btn');
 const sideMenu = document.querySelector('.small-nav');
 const joinUs = document.querySelector('.joinUs');
-const JoinUsForm = document.querySelector('.joinUsForm');
-const closeJoin = document.querySelector('.close-join');
+// const JoinUsForm = document.querySelector('.joinUsForm');
+// const closeJoin = document.querySelector('.close-join');
 
 const inputs = document.querySelectorAll('.join');
 const select = document.querySelector('.choice');
@@ -46,18 +46,51 @@ function addOpacity(includedClass) {
   });
 }
 
-joinUs.addEventListener('click', function () {
-  JoinUsForm.classList.add('showing');
-  JoinUsForm.style.opacity = '1';
-  JoinUsForm.style.display = 'block';
-  removeOpacity('joinUsForm');
+document.addEventListener('DOMContentLoaded', function () {
+  const swiper = new Swiper('.swiper', {
+    direction: 'horizontal', // Ensure it's set to horizontal scrolling
+    loop: true, // Enables infinite looping
+    slidesPerView: 1, // Shows one slide at a time
+    spaceBetween: 25, // Adds spacing between slides
+
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
 });
 
-closeJoin.addEventListener('click', function () {
-  JoinUsForm.classList.remove('showing');
-  JoinUsForm.style.opacity = '0';
-  JoinUsForm.style.display = 'none';
-  addOpacity('joinUsForm');
-  inputs.forEach(input => (input.value = ''));
-  select.value = 'no';
-});
+console.log(typeof Swiper);
+
+// joinUs.addEventListener('click', function () {
+//   JoinUsForm.classList.add('showing');
+//   JoinUsForm.style.opacity = '1';
+//   JoinUsForm.style.display = 'block';
+//   removeOpacity('joinUsForm');
+// });
+
+// closeJoin.addEventListener('click', function () {
+//   JoinUsForm.classList.remove('showing');
+//   JoinUsForm.style.opacity = '0';
+//   JoinUsForm.style.display = 'none';
+//   addOpacity('joinUsForm');
+//   inputs.forEach(input => (input.value = ''));
+//   select.value = 'no';
+// });
