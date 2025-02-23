e4804d61b7130c08f9f78b3e832549f9bc0d28db
hichem Tiba <hichemabderahmenetiba@gmail.com>
2025-02-23 23:08:08 +0100

    more work

--- index.js
+++ index.js
@@ -98,3 +98,22 @@ if (closeJoin) {
     select.value = 'no';
   });
 }
+
+const containers = document.querySelectorAll('.container');
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('show');
+      }
+    });
+  },
+  {
+    threshold: 0.2,
+  }
+);
+
+containers.forEach(container => {
+  observer.observe(container);
+});
